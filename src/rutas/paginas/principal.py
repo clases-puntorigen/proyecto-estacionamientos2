7ed4d146a51d71f4da262fffb372cc219c9e28f6
joseorigen <jose@puntorigen.com>
2025-03-09 16:48:00 -0300

    desarrollo de principal

--- src/rutas/paginas/principal.py
+++ src/rutas/paginas/principal.py
@@ -14,8 +14,8 @@ def principal():
             imagen =  ui.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVpSelqig2f7PiM4oxIzBrLNzo0eI9P-YLCg&s").classes("w-8 h-8 rounded-full items-center")
             with ui.menu():
                 with ui.column().classes("bg-white shadow-lg p-2 rounded-md"):
-                    ui.button("Perfil", on_click=lambda: ui.notify("Opción 1 seleccionada")).classes("w-full")
-                    ui.button("Cerrar Sesion", on_click=lambda: ui.notify("Opción 2 seleccionada")).classes("w-full")
+                    ui.button("Perfil", on_click=lambda: ui.notify("Perfil")).classes("w-full")
+                    ui.button("Cerrar Sesion", on_click=lambda: ui.notify(" Cerrar sesión")).classes("w-full")
         #con chatgpt, para poder saber como hacerlo visible y no...       
                 def toggle_menu():
                     if menu.visible:
@@ -36,15 +36,87 @@ def principal():
 
 
 
-
     #---------------------------------------------------------------------------------------------#
-#estacionamientos por secciones
+#banner
+    def banner():
+        with ui.row().classes("w-full bg-blue-500 text-white p-4 justify-center"):
+              ui.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVpSelqig2f7PiM4oxIzBrLNzo0eI9P-YLCg&s").classes("w-8 h-8 rounded-full items-center")
 
-    with ui.carousel(animated=True, arrows=True, navigation=True).props('height=180px'):
-        with ui.carousel_slide().classes('p-0'):
-            ui.image('https://picsum.photos/id/30/270/180').classes('w-[270px]')
-        with ui.carousel_slide().classes('p-0'):
-            ui.image('https://picsum.photos/id/31/270/180').classes('w-[270px]')
-        with ui.carousel_slide().classes('p-0'):
-            ui.image('https://picsum.photos/id/32/270/180').classes('w-[270px]')
-    
+
+
+
+
+
+
+
+
+
+
+
+#estacionamientos por secciones
+    def estacionamiento():
+        ui.navigate("/estacionamiento") 
+    with ui.column().classes("items-center justify-center w-full space-y-4"):  
+        with ui.row().classes("bg-while-300 p-4 w-[80%] justify-center items-center"): 
+            ui.label("Por Hora").classes("text-xl font-bold mb-4")
+            with ui.row().classes("space-x-4 justify-center"):
+                with ui.column().classes("items-center"):
+                    ui.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVpSelqig2f7PiM4oxIzBrLNzo0eI9P-YLCg&s").classes("w-[350px] h-[350px]")
+                    ui.label("Descripcion del estacionamiento")
+                    ui.button("Reservar", on_click=estacionamiento).classes("bg-blue-500 text-white px-4 py-2 rounded-lg")
+                with ui.column().classes("items-center"):
+                    ui.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVpSelqig2f7PiM4oxIzBrLNzo0eI9P-YLCg&s").classes("w-[350px] h-[350px]") 
+                    ui.label("Descripcion del estacionamiento")
+                    ui.button("Reservar", on_click=estacionamiento).classes("bg-blue-500 text-white px-4 py-2 rounded-lg")
+                with ui.column().classes("items-center"):
+                    ui.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVpSelqig2f7PiM4oxIzBrLNzo0eI9P-YLCg&s").classes("w-[350px] h-[350px]")
+                    ui.label("Descripcion del estacionamiento")
+                    ui.button("Reservar", on_click=estacionamiento).classes("bg-blue-500 text-white px-4 py-2 rounded-lg")
+
+        with ui.row().classes("bg-while-300 p-4 w-[80%] justify-center items-center"): 
+            ui.label("Por Día").classes("text-xl font-bold mb-4")
+            with ui.row().classes("space-x-4 justify-center"):
+                with ui.column().classes("items-center"):
+                    ui.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVpSelqig2f7PiM4oxIzBrLNzo0eI9P-YLCg&s").classes("w-[350px] h-[350px]") 
+                    ui.label("Descripcion del estacionamiento")
+                    ui.button("Reservar", on_click=estacionamiento).classes("bg-blue-500 text-white px-4 py-2 rounded-lg")
+                with ui.column().classes("items-center"):
+                    ui.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVpSelqig2f7PiM4oxIzBrLNzo0eI9P-YLCg&s").classes("w-[350px] h-[350px]")
+                    ui.label("Descripcion del estacionamiento")
+                    ui.button("Reservar", on_click=estacionamiento).classes("bg-blue-500 text-white px-4 py-2 rounded-lg")
+                with ui.column().classes("items-center"):
+                    ui.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVpSelqig2f7PiM4oxIzBrLNzo0eI9P-YLCg&s").classes("w-[350px] h-[350px]")
+                    ui.label("Descripcion del estacionamiento")
+                    ui.button("Reservar", on_click=estacionamiento).classes("bg-blue-500 text-white px-4 py-2 rounded-lg")
+
+        with ui.row().classes("bg-while-300 p-4 w-[80%] justify-center items-center"):  
+            ui.label("Semanal").classes("text-xl font-bold mb-4")
+            with ui.row().classes("space-x-4 justify-center"):
+                with ui.column().classes("items-center"):
+                    ui.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVpSelqig2f7PiM4oxIzBrLNzo0eI9P-YLCg&s").classes("w-[350px] h-[350px]") 
+                    ui.label("Descripcion del estacionamiento")
+                    ui.button("Reservar", on_click=estacionamiento).classes("bg-blue-500 text-white px-4 py-2 rounded-lg")
+                with ui.column().classes("items-center"):
+                    ui.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVpSelqig2f7PiM4oxIzBrLNzo0eI9P-YLCg&s").classes("w-[350px] h-[350px]")
+                    ui.label("Descripcion del estacionamiento")
+                    ui.button("Reservar", on_click=estacionamiento).classes("bg-blue-500 text-white px-4 py-2 rounded-lg")
+                with ui.column().classes("items-center"):
+                    ui.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVpSelqig2f7PiM4oxIzBrLNzo0eI9P-YLCg&s").classes("w-[350px] h-[350px]")
+                    ui.label("Descripcion del estacionamiento")
+                    ui.button("Reservar", on_click=estacionamiento).classes("bg-blue-500 text-white px-4 py-2 rounded-lg")
+
+        with ui.row().classes("bg-while-300 p-4 w-[80%] justify-center items-center"):  
+            ui.label("Mensual").classes("text-xl font-bold mb-4")
+            with ui.row().classes("space-x-4 justify-center"):
+                with ui.column().classes("items-center"):
+                    ui.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVpSelqig2f7PiM4oxIzBrLNzo0eI9P-YLCg&s").classes("w-[350px] h-[350px]") 
+                    ui.label("Descripcion del estacionamiento")
+                    ui.button("Reservar", on_click=estacionamiento).classes("bg-blue-500 text-white px-4 py-2 rounded-lg")
+                with ui.column().classes("items-center"):
+                    ui.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVpSelqig2f7PiM4oxIzBrLNzo0eI9P-YLCg&s").classes("w-[350px] h-[350px]")
+                    ui.label("Descripcion del estacionamiento")
+                    ui.button("Reservar", on_click=estacionamiento).classes("bg-blue-500 text-white px-4 py-2 rounded-lg")
+                with ui.column().classes("items-center"):
+                    ui.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVpSelqig2f7PiM4oxIzBrLNzo0eI9P-YLCg&s").classes("w-[350px] h-[350px]")
+                    ui.label("Descripcion del estacionamiento")
+                    ui.button("Reservar", on_click=estacionamiento).classes("bg-blue-500 text-white px-4 py-2 rounded-lg")
